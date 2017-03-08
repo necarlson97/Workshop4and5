fa3c7667ab0a55d42fe2c8f26c7b0df8e4489fb0
Nils Carlson <nilscarlson1997@gmail.com>
2017-03-07 20:41:16 -0500

    ws06 step 03 breaking the feed apart into subcomponents

---
+++ app/components/commentthread.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class CommentThread extends React.Component {
+  render() {
+    return (
+      <ul className="media-list">
+        {React.Children.map(this.props.children, function(child) {
+          return (
+            <li className="media">
+              {child}
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+}
